102b8704a81fd86e513817133d86abe8639631b9
Antonio Rufino <devs.toni@gmail.com>
2023-03-15 20:48:30 +0100

    Finish register form

---
+++ src/hooks/useForm.js
@@ -0,0 +1,102 @@
+import { useState } from 'react';
+
+
+export const useForm = (initialForm) => {
+
+  const [form, setForm] = useState(initialForm);
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value
+    });
+  };
+
+  const validateForm = (form) => {
+    let errors = {};
+    errors.name = validateName(form.name);
+    errors.username = validateUsername(form.username);
+    errors.email = validateEmail(form.email);
+    errors.password = validatePassword(form.password, form.repeatPassword);
+    return errors;
+  }
+
+  const handleBlur = ({ target }) => {
+    const { name, value } = target;
+    let error = '';
+    if (name === 'password') {
+      error = validateField(name, value, form.repeatPassword);
+    } else if (name === 'repeatPassword') {
+      error = validateField(name, form.password, value);
+    } else {
+      error = validateField(name, value);
+    }
+    if (name === "password" || name === "repeatPassword") {
+      setErrors({ ...errors, password: error });
+      return;
+    }
+    setErrors({ ...errors, [name]: error });
+  }
+
+  const validateField = (field, value1, value2) => {
+    switch (field) {
+      case "name":
+        return validateName(value1)
+      case "email":
+        return validateEmail(value1)
+      case "username":
+        return validateUsername(value1)
+      case "password":
+      case "repeatPassword":
+        return validatePassword(value1, value2)
+      default:
+        break;
+    }
+  }
+
+  const validate = () => {
+    const totalErrors = validateForm(form);
+    setErrors(totalErrors);
+    return errors;
+  }
+
+  return {
+    form, handleChange, handleBlur, validate, errors
+  }
+}
+
+
+const validateName = (value) => {
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let error = '';
+  if (!regexName.test(value.trim()) && value.length > 0) {
+    error = 'Solo acepta letras y espacios en blanco';
+  }
+  return error;
+}
+
+const validateUsername = (value) => {
+  let error = '';
+  return error;
+}
+
+const validateEmail = (value) => {
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  let error = '';
+  if (!regexEmail.test(value.trim()) && value.length > 0) {
+    error = 'El email introducido no és válido';
+  }
+  return error;
+}
+
+const validatePassword = (value1, value2) => {
+  let error = '';
+  if (value1 !== value2) {
+    error = 'Las contraseñas no coinciden'
+  } else if (value1.length < 5 && value1.length > 0) {
+    error = "La contraseña debe tener 5 caracteres como mínimo"
+  }
+  return error;
+}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,53 @@
 import { Button, Label, TextInput } from "flowbite-react";
 import { useLanguage } from "../context/LanguageContext";
 import { FaEye } from 'react-icons/fa';
+import { useForm } from "../hooks/useForm";
+import { Link } from "react-router-dom";
+import { LOGIN } from "../router/paths";
+import Error from "../components/NavBar/Register/Error";
 
-const styleInput = {
-  backgroundColor: "#4B5563",
-  //  border: "1px solid white"
-}
+
 const Register = () => {
 
   const { text } = useLanguage();
+  const { form, handleChange, handleBlur, validate, errors } = useForm({
+    name: '',
+    lastname: '',
+    email: '',
+    username: '',
+    password: '',
+    repeatPassword: ''
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const currentErrors = validate();
+    /*     console.log(Object.keys(currentErrors))
+        console.log(Object.keys(currentErrors).map(v => currentErrors[v].length))
+        console.log(Object.keys(currentErrors).map(v => currentErrors[v].length).filter(e => e !== 0).length) */
+    if (Object.keys(currentErrors).map(v => currentErrors[v].length).filter(e => e !== 0).length === 0) {
+      console.log('Registrando usuario');
+    }
+  }
+
+  const styleInput = (err) => {
+    return {
+      backgroundColor: !err ? "#4B5563" : "#fff",
+      border: err ? "1px solid red" : "1px solid #d0d0d025",
+      color: err ? 'red' : 'white'
+    }
+  };
 
   return (
     <form className="flex flex-col gap-4 max-w-lg px-10 pb-8 pt-7 m-auto rounded-md "
-      style={{ backgroundColor: "#333a44" }}>
+      style={{ backgroundColor: "#333a44" }} onSubmit={handleSubmit}>
       <p className="text-2xl font-semibold">{text.register.title}</p>
       <div>
         <div className="mb-2 block">
           <Label
             color="white"
             htmlFor="registerName"
-            value={text.register.name}
-            className="font-small"
+            value={`* ${text.register.name}`}
           />
         </div>
         <TextInput
@@ -29,9 +56,14 @@ const Register = () => {
           required={true}
           shadow={true}
           color="#fff"
-          style={styleInput}
-          className="border border-gray-500 rounded-lg"
+          style={styleInput(errors?.name)}
+          className={`border border-gray-500 rounded-lg ${errors.name && 'text-red-400 '}`}
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
+        <Error text={errors?.name} />
       </div>
       <div>
         <div className="mb-2 block">
@@ -39,17 +71,19 @@ const Register = () => {
             color="white"
             htmlFor="registerLastname"
             value={text.register.lastname}
-            className="font-small"
           />
         </div>
         <TextInput
           id="registerLastname"
           type="text"
-          required={true}
+          required={false}
           shadow={true}
           color="white"
-          style={styleInput}
+          style={styleInput(errors?.lastname)}
           className="border border-gray-500 rounded-lg"
+          name="lastname"
+          value={form.lastname}
+          onChange={handleChange}
         />
       </div>
       <div>
@@ -57,27 +91,53 @@ const Register = () => {
           <Label
             color="white"
             htmlFor="registerEmail"
-            value={text.register.email}
-            className="font-small"
+            value={`* ${text.register.email}`}
           />
         </div>
         <TextInput
           id="registerEmail"
-          type="email"
+          type="text"
           required={true}
           shadow={true}
           color="white"
-          style={styleInput}
+          style={styleInput(errors?.email)}
           className="border border-gray-500 rounded-lg"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
+        <Error text={errors?.email} />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label
+            color="white"
+            htmlFor="registerUsername"
+            value={`* ${text.register.username}`}
+          />
+        </div>
+        <TextInput
+          id="registerUsername"
+          type="text"
+          required={true}
+          shadow={true}
+          color="white"
+          style={styleInput(errors?.username)}
+          className="border border-gray-500 rounded-lg"
+          name="username"
+          value={form.username}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        <Error text={errors?.username} />
       </div>
       <div className="relative">
         <div className="mb-2 block">
           <Label
             color='white'
             htmlFor="registerPassword"
-            value={text.register.password}
-            className="font-small"
+            value={`* ${text.register.password}`}
           />
         </div>
         <TextInput
@@ -86,28 +146,36 @@ const Register = () => {
           required={true}
           shadow={true}
           color="white"
-          style={styleInput}
+          style={styleInput(errors?.password)}
           className="border border-gray-500 rounded-lg"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
+        <Error text={errors?.password} />
         <FaEye className="absolute right-4 top-11 opacity-60 hover:opacity-100 cursor-pointer" />
       </div>
       <div>
         <div className="mb-2 block">
           <Label
             color='white'
-            htmlFor="repeatPassword"
-            value={text.register.repeat}
-            className="font-small"
+            htmlFor="repeatPass"
+            value={`* ${text.register.repeat}`}
           />
         </div>
         <TextInput
-          id="repeatPassword"
+          id="repeatPass"
           type="password"
           required={true}
           shadow={true}
           color="white"
-          style={styleInput}
+          style={styleInput(errors?.password)}
           className="border border-gray-500 rounded-lg"
+          name="repeatPassword"
+          value={form.repeatPassword}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
       </div>
       <div className="gap-2 text-left">
@@ -125,7 +193,7 @@ const Register = () => {
       </Button>
       <div className="flex text-sm mt-1">
         <span className="w-fit">{text.register.already}</span>
-        <a href="#" className="ml-2 text-pink-300 hover:underline">{text.register.login}</a>
+        <Link to={`/${LOGIN}`} className="ml-2 text-pink-300 hover:underline">{text.register.login}</Link>
       </div>
     </form>
   )
