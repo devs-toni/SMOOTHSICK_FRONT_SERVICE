3775efebf10a1df6c6f1f28ecc0c2d7aed87948f
Sergi <scsanz87@gmail.com>
2023-03-17 09:32:51 +0100

    account-settings-changes

--- src/components/config/Accountsettings.jsx
+++ src/components/config/Accountsettings.jsx
@@ -1,18 +1,57 @@
 import React from 'react'
 
 const Accountsettings = () => {
-  return (
-    <div>Accountsettings
-         <label for="email-address-icon" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
-    <div className="relative">
-      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
-      </div>
-      <input type="text" id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" />
-    </div>
-  
-    </div>
-  )
+    return (
+        <>
+
+            <div className="flex items-center flex-col pt-20 gap-7 flex-wrap">
+                <div className='flex flex-row justify-around border border-whiteflex w-screen'>
+                    <div>
+                        <span> </span>
+                        <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Purple to Pink</button>
+
+                    </div>
+                    <div>
+                        <h1>User</h1>
+                        <span>Contact</span>
+                        <span>Billing Adress</span>
+                    </div>
+                    <div>
+                        <h1>User</h1>
+                        <span>Contact</span>
+                        <span>Billing Adress</span>
+                    </div>
+                </div>
+                
+            </div>
+            <div className='flex flex-row justify-around border border-whiteflex w-screen'>
+                <div>
+                    <p>Plant Details</p>
+                </div>
+                <div><p>Type Suscription</p></div>
+                <div><p>Link to change Suscription</p></div>
+            </div>
+            <div className='flex flex-row justify-around border border-whiteflex w-screen'>
+                <div><p>Security and Privacy</p></div>
+                <div><p>Security Options</p></div>
+                <div><p>Link to Security options</p></div>
+            </div>
+            <div>
+                asdasdasd
+            </div>
+            <div>
+                f234radasd
+            </div>
+            <div>
+                aq34qasdad
+            </div>
+
+
+      
+
+        </>
+
+    )
 }
 
 export default Accountsettings
