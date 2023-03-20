6cd621332efc7458738156b85428f8504243f2ca
Sergi <scsanz87@gmail.com>
2023-03-20 12:37:35 +0100

    feat acc-settings-desing-done

--- src/components/AccountSettings/Accountsettings.jsx
+++ src/components/AccountSettings/Accountsettings.jsx
@@ -1,50 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useLanguage } from '../../context/LanguageContext'
 
 
 const Accountsettings = () => {
+    const { text } = useLanguage()
     return (
 
 
-        <div className="flex justify-center items-center flex-col pt-20 gap-7 ">
-            <div className='flex flex-row justify-around border border-whiteflex w-full'>
-                <div>
-                    <span>Account Settings</span>
-                    <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Prueba</button>
+        <div className="flex justify-around items-center flex-col pt-56 gap-7 max-w-5xl	m-auto mt-10 p-10 md:max-w-xl lg:max-w-3xl xl:max-w-6xl ">
+            <div className='grid grid-cols-3 justify-start items-center gap-7 border border-t-transparent border-l-transparent border-r-transparent focus:border-transparent focus:ring-0 border-b-1 border-neutral-500  w-full'>
+                <div className='flex flex-col'>
+                    <span> {text.account.account} </span>
+                    <button type="button" className="text-white h-16 w-32 bg-gradient-to-r from-purple-500 to-pink-500  hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Cancel Subscription</button>
                 </div>
                 <div className='flex flex-col'>
-                    <h1>User</h1>
-                    <span>Contact</span>
-                    <span>Billing Adress</span>
+                    <h1> {text.account.user}</h1>
+                    <div>
+                        <p>Name: <span>Antonio</span> </p>
+                        <p>Email: <span>rufigo@gmail.com</span></p>
+                    </div>
                 </div>
                 <div className='flex flex-col'>
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Change password </a><br></br>
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Change direction </a><br></br>
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Change mobile phone </a><br></br>
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Payment information </a><br></br>
+                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{text.account.password}</a><br></br>
+                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{text.account.edit} </a><br></br>
+                   
                 </div>
             </div>
 
-            <div className='flex flex-row justify-around border border-whiteflex w-full h-40'>
+            <div className='grid grid-cols-3 justify-start items-center gap-7 border border-t-transparent border-l-transparent border-r-transparent focus:border-transparent focus:ring-0 border-b-1 border-neutral-500 w-full h-40'>
                 <div>
-                    <p>Plant Details</p>
+                    <p>{text.account.plan}</p><br></br>
                 </div>
-                <div><p>Type Suscription</p></div>
+                <div><p>{text.account.subscription} </p><br></br></div>
                 <div><a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Change the plan</a></div>
             </div>
-            <div className='flex flex-row justify-around border border-whiteflex w-full h-50'>
-                <div><p>Security and Privacy</p></div>
+            <div className='grid grid-cols-3 justify-start items-center pb-6 gap-7 border border-t-transparent border-l-transparent border-r-transparent focus:border-transparent focus:ring-0 border-b-1 border-neutral-500 w-full h-1/3'>
+                <div className=""><p>Security and Privacy</p></div>
                 <div><p>Security Options</p></div>
                 <div><a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Security options </a></div>
             </div>
-            <div>
-                asdasdasd
-            </div>
-            <div>
-                f234radasd
-            </div>
-            <div>
-                aq34qasdad
-            </div>
+           
         </div>
 
 
