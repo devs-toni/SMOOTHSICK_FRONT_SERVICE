34b8767983c8ead7f0ec57acbacc758ff91ba239
Sergi <scsanz87@gmail.com>
2023-03-22 16:26:53 +0100

    feat soundbox design

--- src/components/Home/Soundbox.jsx
+++ src/components/Home/Soundbox.jsx
@@ -2,16 +2,20 @@ import React from "react"
 
 const Soundbox = ({ id, name, image, artist }) => {
   return (
-    <div className="bg-chart box-shadow mt-2 px-5 py-3 w-3/6 text-left mr-7 rounded-md">
+    <div className="bg-chart box-shadow mt-2 p-5 w-5/6 text-left mr-7 rounded-md papel">
 
 
-      <div className="flex flex-row justify-between text-center">
+      <div className="flex flex-row justify-between text-center mb-3 w-full h-full">
         <img src={image} alt={name} className="rounded-lg w-full h-full" />
       </div>
 
+      <div className="mt-2 pl-1">
 
-      <p className="mt-2 font-medium text-md">{name}</p>
-      <p className="mt-2 text-sm">{artist}</p>
+        <p className="font-medium text-md mb-2">{name}</p>
+        <p className="text-sm">{artist}</p>
+
+
+      </div>
 
 
 
