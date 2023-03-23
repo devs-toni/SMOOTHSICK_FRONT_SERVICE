cd131a921ec6f1b444b716a732eb04ac491e5c65
Antonio Rufino <devs.toni@gmail.com>
2023-03-23 18:06:38 +0100

    Start detail page

---
+++ src/components/DetailsComponents/Details.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const Details = () => {
+
+  const { type, id } = useParams();
+
+  return (
+    <div className='h-full'>
+      <div className='w-4/6 pt-20 m-auto'>
+        Details
+        <p>{type}</p>
+        <p>{id}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Details
