2fca904da6edcc77a35e27c5135ff1d8da026b86
Sergi <scsanz87@gmail.com>
2023-05-18 21:05:33 +0200

    MySongs.jsx Translations finished & Json

--- src/components/MySongs/MySongs.jsx
+++ src/components/MySongs/MySongs.jsx
@@ -28,14 +28,14 @@ const MySongs = () => {
             <div className="w-24 h-24 md:w-36 md:h-36 lg:h-60 lg:w-60 flex flex-col text-xs md:text-lg rounded-lg items-center justify-center bg-gradient-to-b from-[#ef5567]  transition duration-700 hover:bg-deezer hover:cursor-pointer">
               <AiOutlinePlus size={32} color="black" className="hover:rounded-full  hover:bg-opacity-10 hover:bg-slate-900 " />
             </div>
-            <span>Upload your songs</span>
+            <span>{text.playlists.upload}</span>
           </div>
           <div className="z-5 flex flex-col h-25 text-center justify-center w-8/6 min-w-[100%] ">
             <div className='flex items-center text-xs md:text-sm lg:text-lg justify-between border-b border-line-section'>
               <p className="hidden md:block w-1/12">#</p>
               <p className="w-2/12">{text.liked.track}</p>
               <p className="w-2/12"></p>
-              <p className="w-3/12">Options</p>
+              <p className="w-3/12">{text.categories.options}</p>
               <p className="hidden md:block w-3/12">{text.categories.artist}</p>
               <p className="hidden lg:block w-2/12">{text.liked.gender}</p>
               <BsClock className='w-2/12' />
